23f812d1c5eb9836c507b710a9951448dde3d709
nikitaabramov.dev <abramov.mykyta@gmail.com>
2021-12-20 13:40:13 +0200

    add another gridsome server js

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,13 +5,14 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const axios = require('axios')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = function (api) {
   api.chainWebpack((config, { isServer }) => {
     if (isServer) {
       config.externals([
         nodeExternals({
-          whitelist: [/^vuetify/]
+          allowlist: [/^vuetify/]
         })
       ])
     }
